37485969786b61034e212a8073be33b7be142ce5
David <davidkimighty@gmail.com>
2023-04-25 17:13:51 +0900

    cloud code cli automative login

--- Sample_UGS_CloudCode/deploy.sh
+++ Sample_UGS_CloudCode/deploy.sh
@@ -1,9 +1,6 @@
 ugs config set project-id 6ce8abee-1a20-49b4-bcf0-59dedca0de15
 ugs config set environment-name development
 
-# 3fea8b21-445c-4b38-9589-b29794ab0825
-# kLswJyIJ7uSA3taw4e-85hrzqxLgeCsM
-
 # Remove the previous module
 rm Sample_UGS_CloudCode.ccm
 
